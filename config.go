0189fa8e6e2935b255ac0d54149bd9fe6fec6829
simone.tiberti <simone.tiberti@gpagroup.it>
2025-03-31 16:34:13 +0200

    pool

--- config.go
+++ config.go
@@ -18,19 +18,13 @@ import (
 )
 
 type Config struct {
-	Server        string `yaml:"server" mapstructure:"server" json:"server"`
-	Database      string `yaml:"database" mapstructure:"database" json:"database"`
-	Username      string `yaml:"username" mapstructure:"username" json:"username"`
-	Password      string `yaml:"password" mapstructure:"password" json:"password"`
-	AuthMechanism string `mapstructure:"authMechanism" json:"authMechanism" yaml:"authMechanism"`
-	AuthDB        string `yaml:"authdb" mapstructure:"authdb" json:"authdb"`
-	Pool          *struct {
-		MinConn               *uint64        `mapstructure:"min-conn" json:"min-conn" yaml:"min-conn"`
-		MaxConn               *uint64        `mapstructure:"max-conn" json:"max-conn" yaml:"max-conn"`
-		MaxWaitTime           *time.Duration `mapstructure:"max-wait-time" json:"max-wait-time" yaml:"max-wait-time"`
-		MaxConnectionIdleTime *time.Duration `mapstructure:"max-conn-idle-time" json:"max-conn-idle-time" yaml:"max-conn-idle-time"`
-		MaxConnecting         *uint64        `mapstructure:"max-connecting" json:"max-connecting" yaml:"max-connecting"`
-	}
+	Server                 string         `yaml:"server" mapstructure:"server" json:"server"`
+	Database               string         `yaml:"database" mapstructure:"database" json:"database"`
+	Username               string         `yaml:"username" mapstructure:"username" json:"username"`
+	Password               string         `yaml:"password" mapstructure:"password" json:"password"`
+	AuthMechanism          string         `mapstructure:"authMechanism" json:"authMechanism" yaml:"authMechanism"`
+	AuthDB                 string         `yaml:"authdb" mapstructure:"authdb" json:"authdb"`
+	Pool                   *Pool          `yaml:"pool" mapstructure:"pool" json:"pool" yaml:"pool"`
 	WriteConcern           string         `mapstructure:"write-concern" json:"write-concern" yaml:"write-concern"`
 	ReadConcern            string         `mapstructure:"read-concern" json:"read-concern" yaml:"read-concern"`
 	OperationTimeout       *time.Duration `mapstructure:"operation-timeout" json:"operation-timeout" yaml:"operation-timeout"`
@@ -52,6 +46,13 @@ type TLSConfig struct {
 	SkipVerify bool   `json:"skip-verify" mapstructure:"skip-verify" yaml:"skip-verify"`
 }
 
+type Pool struct {
+	MinConn               *uint64        `mapstructure:"min-conn" json:"min-conn" yaml:"min-conn"`
+	MaxConn               *uint64        `mapstructure:"max-conn" json:"max-conn" yaml:"max-conn"`
+	MaxWaitTime           *time.Duration `mapstructure:"max-wait-time" json:"max-wait-time" yaml:"max-wait-time"`
+	MaxConnectionIdleTime *time.Duration `mapstructure:"max-conn-idle-time" json:"max-conn-idle-time" yaml:"max-conn-idle-time"`
+	MaxConnecting         *uint64        `mapstructure:"max-connecting" json:"max-connecting" yaml:"max-connecting"`
+}
 type MetricConfig struct {
 	Buckets struct {
 		ConnectionTimeReady       *[]float64 `mapstructure:"connection-time-ready" json:"connection-time-ready" yaml:"connection-time-ready"`
